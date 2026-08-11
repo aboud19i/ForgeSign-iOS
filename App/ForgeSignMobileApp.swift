7cb17b9fde4cc5f5ea38dd61806e30d3d428cc41
hggdet <id9871456@gmail.com>
2026-08-11 20:07:45 +0300

    Fix: inject ProfileStore as EnvironmentObject so AppsView gets profileStore

--- App/ForgeSignMobileApp.swift
+++ App/ForgeSignMobileApp.swift
@@ -4,12 +4,14 @@ import SwiftUI
 struct ForgeSignMobileApp: App {
     @StateObject private var sourceStore = SourceStore()
     @StateObject private var certStore = CertificateStore()
+    @StateObject private var profileStore = ProfileStore()
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(sourceStore)
                 .environmentObject(certStore)
+                .environmentObject(profileStore)
         }
     }
 }
